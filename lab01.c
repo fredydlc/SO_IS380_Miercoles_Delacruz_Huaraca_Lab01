a23110c99ee93a6c2208bb8b36c426ef0e5bd5f7
fredydlc <fredy.delacruz.27@unsch.edu.pe>
2026-09-14 23:07:43 -0500

    feat(lab01): estructura base de main e impresion del encabezado del alumno

--- lab01.c
+++ lab01.c
@@ -6,3 +6,14 @@ struct Proceso {
     char nombre[20];
     int prioridad; // 1 (alta) a 5 (baja)
 };
+int main() {
+    int n = 5;
+    struct Proceso procesos[5];
+
+    printf("===================================================\n");
+    printf("   SISTEMAS OPERATIVOS IS-380 - SIMULADOR SCHEDULER\n");
+    printf("   Estudiante: Fredy de la Cruz Huaraca\n");
+    printf("===================================================\n\n");
+
+    return 0;
+}
